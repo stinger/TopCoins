6b10f6a67bf6c0a02e3d3c9af036f2fea04d28f8
Ilian Konchev <konchev@gmail.com>
2025-02-19 19:49:10 +0200

    Initial Commit

---
+++ TopCoins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TopCoins
+//
+//  Created by Ilian Konchev on 19.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TopCoinsUITests/TopCoinsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TopCoinsUITestsLaunchTests.swift
+//  TopCoinsUITests
+//
+//  Created by Ilian Konchev on 19.02.25.
+//
+
+import XCTest
+
+final class TopCoinsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
